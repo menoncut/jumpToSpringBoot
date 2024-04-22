b418542a941bf9209ae0f25b36731f6bf30c65ad
menoncut <133994207+menoncut@users.noreply.github.com>
2024-04-22 15:08:38 +0900

    jumpToSpringBoot
    jumpToSpringBoot

---
+++ sbb/answer/Answer.java
@@ -0,0 +1,69 @@
+package com.example.sbb.answer;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import com.example.sbb.question.Question;
+import com.example.sbb.user.SiteUser;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(columnDefinition = "TEXT")
+	private String content;
+	
+	private LocalDateTime createDate;
+	
+	private LocalDateTime updateDate;
+	
+	@ManyToOne
+	private Question question;
+	
+	@ManyToOne
+	private SiteUser author;
+	
+	@ManyToMany
+	Set<SiteUser> voter;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
